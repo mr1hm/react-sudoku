0fc9ee143237525c7038fe4f5c2c7e65367a727e
Kevin <kevinihm@gmail.com>
2020-03-14 18:20:51 -0700

    added space for footer

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import Header from './layout/header';
+import Footer from './layout/footer';
 import GameBoard from './game-board';
 import InputSelection from './input-selection';
 
