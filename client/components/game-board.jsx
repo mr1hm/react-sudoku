82647fec689bbe3db8ab02024bded04e65f7dbf6
Kevin <kevinihm@gmail.com>
2020-03-20 16:46:37 -0700

    renamed loop variables

--- client/components/game-board.jsx
+++ client/components/game-board.jsx
@@ -101,28 +101,28 @@ export default class GameBoard extends Component {
     this.setState({ solution: gameBoard })
     return gameBoard;
 
-    function isValid(board, row, col, k) {
+    function isValid(board, row, col, value) {
       for (let i = 0; i < 9; i++) {
         const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
         const n = 3 * Math.floor(col / 3) + i % 3;
-        if (board[row][i] == k || board[i][col] == k || board[m][n] == k) {
+        if (board[row][i] == value || board[i][col] == value || board[m][n] == value) {
           return false;
         }
       }
       return true;
     }
 
-    function sudokuSolver(data) {
+    function sudokuSolver(randomBoard) {
       for (let row = 0; row < 9; row++) {
         for (let col = 0; col < 9; col++) {
-          if (data[row][col] == '') {
-            for (let k = 1; k <= 9; k++) {
-              if (isValid(data, row, col, k)) {
-                data[row][col] = `${k}`;
-                if (sudokuSolver(data)) {
+          if (randomBoard[row][col] == '') {
+            for (let value = 1; value <= 9; value++) {
+              if (isValid(randomBoard, row, col, value)) {
+                randomBoard[row][col] = `${value}`;
+                if (sudokuSolver(randomBoard)) {
                   return true;
                 } else {
-                  data[row][col] = '';
+                  randomBoard[row][col] = '';
                 }
               }
             }
